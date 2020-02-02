1b4de6582c1db0b5c499f0de37a3e3cc3d7feec1
Niayaa <yongshi0627@gmail.com>
2020-02-01 22:36:53 -0500

    main_page
    this is the main_page and game mode choose page

---
+++ Ice Battle/Assets/script/Devaloper_shown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Devaloper_shown : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+    public void onShown()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void close()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ice Battle/Assets/script/Menu_button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void start_button(string scenesName)
+    {
+        SceneManager.LoadScene(scenesName);
+    }
+
+    
+}
